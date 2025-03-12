81e8e9f9c9c6adfc5de55862b58dad0c6592c385
Mesropian <mesropyan0206@gmail.com>
2025-03-12 13:15:27 +0400

    controller aded

---
+++ controllers/projectController.js
@@ -0,0 +1,22 @@
+import Project from "../models/Project.js";
+
+// get all projects
+export const getProjects = async (req, res) => {
+  try {
+    const projects = await Project.find();
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ message: " Server Error" });
+  }
+};
+
+// Create new Project
+export const createProject = async (req, res) => {
+  try {
+    const newProject = new Project(req.body);
+    await newProject.save();
+    res.status(201).json(newProject);
+  } catch (error) {
+    res.status(400).json({ message: "Error creating project" });
+  }
+};
